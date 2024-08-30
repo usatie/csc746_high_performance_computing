f4d76b770c0e543e38d628f815d7ca313fdf3ec3
Shun Usami <usatie@gmail.com>
2024-08-29 18:45:24 -0700

    Implement Indirect Sum

--- sum_indirect.cpp
+++ sum_indirect.cpp
@@ -12,13 +12,23 @@ void
 setup(int64_t N, uint64_t A[])
 {
    printf(" inside sum_indirect problem_setup, N=%lld \n", N);
+   for (int64_t i = 0; i < N; i++)
+   {
+      A[i] = lrand48() % N;
+   }
 }
 
 int64_t
 sum(int64_t N, uint64_t A[])
 {
    printf(" inside sum_indirect perform_sum, N=%lld \n", N);
+   int64_t sum = 0;
+   for (int64_t i = 0; i < N; i++)
+   {
+      int64_t indirect_index = A[i];
+      sum += A[indirect_index];
+   }
 
-   return 0;
+   return sum;
 }
 
