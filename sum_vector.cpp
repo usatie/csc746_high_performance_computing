26f062fe2f9a181500afc465d26ae74234ff2e96
Shun Usami <usatie@gmail.com>
2024-08-29 18:44:11 -0700

    Implement Vector Sum

--- sum_vector.cpp
+++ sum_vector.cpp
@@ -12,13 +12,22 @@ void
 setup(int64_t N, uint64_t A[])
 {
    printf(" inside sum_vector problem_setup, N=%lld \n", N);
+   for (int64_t i = 0; i < N; i++)
+   {
+      A[i] = i;
+   }
 }
 
 int64_t
 sum(int64_t N, uint64_t A[])
 {
    printf(" inside sum_vector perform_sum, N=%lld \n", N);
+   int64_t sum = 0;
+   for (int64_t i = 0; i < N; i++)
+   {
+      sum += A[i];
+   }
 
-   return 0;
+   return sum;
 }
 
