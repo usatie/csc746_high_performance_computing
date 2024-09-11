ee59779efb4dcf814cd3d87b4383c925b6ff2a3b
Shun Usami <usatie@gmail.com>
2024-09-10 18:53:19 -0700

    Implemented dgemm-basic

--- dgemm-basic.cpp
+++ dgemm-basic.cpp
@@ -8,4 +8,11 @@ const char *dgemm_desc = "Basic implementation, three-loop dgemm.";
  */
 void square_dgemm(int n, double *A, double *B, double *C) {
   // insert your code here: implementation of basic matrix multiple
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      for (int k = 0; k < n; k++) {
+        C[i + j * n] += A[i + k * n] * B[k + j * n];
+      }
+    }
+  }
 }
