5a43c0d78c44e38a95af24cc73e067f9c1f8a03b
Shun Usami <usatie@gmail.com>
2024-09-27 23:04:49 -0700

    Implement opemMP parallelization for vmmul

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -15,16 +15,27 @@ const char *dgemv_desc = "OpenMP dgemv.";
 void my_dgemv(int n, double *A, double *x, double *y) {
 
 #pragma omp parallel
-  {
-    int nthreads = omp_get_num_threads();
-    int thread_id = omp_get_thread_num();
-    printf("my_dgemv(): Hello world: thread %d of %d checking in. \n",
-           thread_id, nthreads);
-    printf("my_dgemv(): For actual timing runs, please comment out these "
-           "printf() and omp_get_*() statements. \n");
-  }
+  //{
+  //  int nthreads = omp_get_num_threads();
+  //  int thread_id = omp_get_thread_num();
+  //  printf("my_dgemv(): Hello world: thread %d of %d checking in. \n",
+  //         thread_id, nthreads);
+  //  printf("my_dgemv(): For actual timing runs, please comment out these "
+  //         "printf() and omp_get_*() statements. \n");
+  //}
 
   // insert your dgemv code here. you may need to create additional parallel
   // regions, and you will want to comment out the above parallel code block
   // that prints out nthreads and thread_id so as to not taint your timings
+  {
+    int nthreads = omp_get_num_threads();
+    int thread_id = omp_get_thread_num();
+    int istart = (thread_id * n) / nthreads;
+    int iend = ((thread_id + 1) * n) / nthreads;
+    for (int i = istart; i < iend; i++) {
+      for (int j = 0; j < n; j++) {
+        y[i] += A[i * n + j] * x[j];
+      }
+    }
+  }
 }
