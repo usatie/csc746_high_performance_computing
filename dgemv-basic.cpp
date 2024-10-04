15657bbf4f386c56cab72214594d63b04ed12a75
Shun Usami <usatie@gmail.com>
2024-10-04 00:35:30 -0700

    Add loop ordering to dgemv-basic and dgemv-vectorized

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -8,8 +8,13 @@ const char *dgemv_desc = "Basic implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double *A, double *x, double *y) {
   // insert your code here: implementation of basic matrix multiply
+#ifdef LOOP_IJ
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
+#elif LOOP_JI
+  for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++) {
+#endif
       y[i] += A[i * n + j] * x[j];
     }
   }
--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -8,8 +8,13 @@ const char *dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double *A, double *x, double *y) {
   // insert your code here: implementation of vectorized vector-matrix multiply
+#ifdef LOOP_IJ
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
+#elif LOOP_JI
+  for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++) {
+#endif
       y[i] += A[i * n + j] * x[j];
     }
   }
