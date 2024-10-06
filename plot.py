e1e4564514749ffe5db367a9984385adbc88af73
Shun Usami <usatie@gmail.com>
2024-10-05 22:26:58 -0700

    Add speedup plot

--- plot.py
+++ plot.py
@@ -71,6 +71,57 @@ def save_elapsed_time(
     plt.clf()
 
 
+def save_speedup(
+    plot_fname,
+    plot_title,
+    problem_sizes,
+    serial_runtime,
+    parallel_runtime,
+    num_threads,
+):
+    plt.title(plot_title)
+
+    xlocs = [i for i in range(len(problem_sizes))]
+
+    plt.xticks(xlocs, problem_sizes)
+
+    ideal_speedup = [num_threads for i in range(len(problem_sizes))]
+    print("ideal speedup =", ideal_speedup)
+    print("serial runtime =", serial_runtime)
+    print("parallel runtime =", parallel_runtime)
+    actual_speedup = [
+        serial / parallel for serial, parallel in zip(serial_runtime, parallel_runtime)
+    ]
+
+    plt.plot(ideal_speedup, "k--")
+    plt.plot(actual_speedup, PLOT_FORMATS[0])
+    # Annotate data points
+    for i, (size, speedup) in enumerate(zip(problem_sizes, actual_speedup)):
+        # If there are too close data points, move one of them to avoid overlap
+        # the distance should be scaled in log scale
+        plt.annotate(
+            round(speedup, 1),
+            (i, speedup),
+            textcoords="offset points",
+            xytext=(0, 5),
+            ha="center",
+            size=8,
+        )
+
+    # plt.xscale("log")
+    # plt.yscale("log")
+
+    plt.xlabel("Matrix Size (N)")
+    plt.ylabel("Speedup")
+
+    plt.legend(["ideal speedup", "actual speedup"], loc="best")
+
+    plt.grid(axis="both")
+
+    # save the figure before trying to show the plot
+    plt.savefig(RESULT_DIR + plot_fname, dpi=300)
+    plt.clf()
+
 def calc_mvmul_mflops(elapsed_time, problem_size):
     return 2 * pow(problem_size, 2) / elapsed_time / 1_000_000
 
@@ -127,6 +178,15 @@ def save_mflops(
     plt.clf()
 
 
+# Memory Access: 2 * N + 2 * N^2
+# double: 8 bytes
+# Memory Bandwidth = [Memory Access] * 8 bytes / elapsed_time / 1_000_000_000
+def calc_mvmul_memory_bw(elapsed_time, problem_size):
+    return (
+        (2 * problem_size + 2 * pow(problem_size, 2)) * 8 / elapsed_time / 1_000_000_000
+    )
+
+
 def save_memory_bandwidth(
     plot_fname,
     plot_title,
@@ -141,10 +201,7 @@ def save_memory_bandwidth(
     plt.xticks(xlocs, problem_sizes)
 
     code_memory_bw = [
-        [
-            size * size * 8 / t / 1_000_000_000
-            for (t, size) in zip(code_time, problem_sizes)
-        ]
+        [calc_mvmul_memory_bw(t, size) for (t, size) in zip(code_time, problem_sizes)]
         for code_time in code_times
     ]
     for memory_bw_arr, fmt in zip(code_memory_bw, PLOT_FORMATS):
@@ -234,6 +291,14 @@ def save_figures():
         ],
         var_names[1:],
     )
+    # MFLOPs of blas, basic, vectorized
+    save_mflops(
+        "MFLOPs-blas-basic-vectorized.png",
+        "MFLOP/s (blas, basic, vectorized)",
+        problem_sizes,
+        [blas_time, basic_time, vectorized_time],
+        var_names[1:4],
+    )
     save_memory_bandwidth(
         "Memory_Bandwidth.png",
         "Memory Bandwidth (GB/s)",
@@ -248,6 +313,32 @@ def save_figures():
             openmp_64_time,
         ],
         var_names[1:],
+    )
+
+    # speedup
+    save_speedup(
+        "Speedup-4-threads.png",
+        "Speedup (4 threads)",
+        problem_sizes,
+        openmp_1_time,
+        openmp_4_time,
+        4,
+    )
+    save_speedup(
+        "Speedup-16-threads.png",
+        "Speedup (16 threads)",
+        problem_sizes,
+        openmp_1_time,
+        openmp_16_time,
+        16,
+    )
+    save_speedup(
+        "Speedup-64-threads.png",
+        "Speedup (64 threads)",
+        problem_sizes,
+        openmp_1_time,
+        openmp_64_time,
+        64,
     )
 
 
