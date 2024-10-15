93930e2ccf85d6341c1b008932b5c2dd762902a5
SHUN USAMI <usatie@login03.chn.perlmutter.nersc.gov>
2024-10-15 12:28:43 -0700

    Modify benchmark script to specify log directory

--- remote-bench.sh
+++ remote-bench.sh
@@ -17,28 +17,17 @@ unset LUA_CPATH
 export SBATCH_ACCOUNT=m3930
 export SALLOC_ACCOUNT=m3930
 
-# Generate the Slurm batch script below with the here document,
-# then when sbatch the script later, the user env set up above will run on the login node
-# instead of on a head compute node (if included in the Slurm batch script),
-# and inherited into the batch job.
-COMMIT_HASH=$(git rev-parse --short HEAD) # get the short commit hash
-cat << 'EOF' > job-benchmark.sh
-#!/bin/bash -l
-#SBATCH --constraint=cpu
-#SBATCH --cpus-per-task=1
-#SBATCH --qos=debug
-#SBATCH --nodes=1
-#SBATCH --account=m3930
-#SBATCH --perf=generic
-#SBATCH --time=00:05:00
-#SBATCH --job-name=job-benchmark
-#SBATCH --output=job-benchmark.o.${COMMIT_HASH}.%j
-#SBATCH --error=job-benchmark.e.${COMMIT_HASH}.%j
-
-./job-blas
-./job-basic-omp
-./job-blocked-omp
-EOF
-
 # Now submit the batch job
-sbatch job-benchmark.sh
+COMMIT_HASH=$(git rev-parse --short HEAD) # get the short commit hash
+sbatch \
+	--output=../log/job-blas.o.${COMMIT_HASH}.%j \
+ 	--error=../log/job-blas.e.${COMMIT_HASH}.%j \
+	job-blas
+sbatch \
+	--output=../log/job-basic-omp.o.${COMMIT_HASH}.%j \
+ 	--error=../log/job-basic-omp.e.${COMMIT_HASH}.%j \
+	job-basic-omp
+sbatch \
+	--output=../log/job-blocked-omp.o.${COMMIT_HASH}.%j \
+ 	--error=../log/job-blocked-omp.e.${COMMIT_HASH}.%j \
+	job-blocked-omp
