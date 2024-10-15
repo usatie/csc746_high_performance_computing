a8c8ce233add39b147a57912572c243b0c774536
Shun Usami <usatie@gmail.com>
2024-10-15 10:52:29 -0700

    Implement Basic OMP Matrix Multiply

--- dgemm-basic-omp.cpp
+++ dgemm-basic-omp.cpp
@@ -13,10 +13,22 @@ const char* dgemm_desc = "Basic implementation, OpenMP-enabled, three-loop dgemm
 void square_dgemm(int n, double* A, double* B, double* C) 
 {
    // insert your code here: implementation of basic matrix multiply with OpenMP parallelism enabled
-
    // be sure to include LIKWID_MARKER_START(MY_MARKER_REGION_NAME) inside the block of parallel code,
    // but before your matrix multiply code, and then include LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME)
    // after the matrix multiply code but before the end of the parallel code block.
-
-   std::cout << "Insert your basic matrix multiply, openmp-parallel edition here " << std::endl;
+#pragma omp parallel for
+   for (int i = 0; i < n; ++i) {
+#ifdef LIKWID_PERFMON
+      LIKWID_MARKER_START(MY_MARKER_REGION_NAME);
+#endif
+      for (int j = 0; j < n; ++j) {
+         double cij = C[i*n+j];   /* cij = C[i][j] */
+         for( int k = 0; k < n; k++ )
+            cij += A[i*n+k] * B[k*n+j]; /* cij += A[i][k]*B[k][j] */
+         C[i*n+j] = cij; /* C[i][j] = cij */
+      }
+#ifdef LIKWID_PERFMON
+      LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME);
+#endif
+   }
 }
