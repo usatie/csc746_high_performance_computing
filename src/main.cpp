dd2793d8f3389149d28275d013e8965d5147fd2a
Shun Usami <usatie@gmail.com>
2024-10-16 14:47:58 -0700

    Add color header file and refactor color output

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "vec3.h"
+#include "color.h"
 
 int main() {
     // Image
@@ -12,15 +13,8 @@ int main() {
     for (int j = image_height - 1; j >= 0; j--) {
         std::clog << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            write_color(std::cout, pixel_color);
         }
     }
     std::clog << "\nDone.\n";
