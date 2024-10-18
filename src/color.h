58a8289cc54b5f6b3f6a3f3ad53d89e81a5c3c4a
Shun Usami <usatie@gmail.com>
2024-10-18 01:06:41 -0700

    Modify color space from linear to gamma

--- src/color.h
+++ src/color.h
@@ -6,10 +6,22 @@
 
 using color = vec3;
 
+// We use gamme 2 as our transformation
+inline double linear_to_gamma(double linear_component) {
+  if (linear_component > 0)
+    return std::sqrt(linear_component);
+  return 0;
+}
+
 void write_color(std::ostream &out, color pixel_color) {
   auto r = pixel_color.x();
   auto g = pixel_color.y();
   auto b = pixel_color.z();
+
+  // Apply a linear to gamma transformation.
+  r = linear_to_gamma(r);
+  g = linear_to_gamma(g);
+  b = linear_to_gamma(b);
 
   // Translate the [0,1] component values to the byte range [0,255].
   static const interval intensity(0.0, 0.999);
