5abeebc604675a0d02a239ef3ff640fafe2f92c8
Shun Usami <usatie@gmail.com>
2024-10-18 01:26:48 -0700

    Add fuzziness to metal

--- src/material.h
+++ src/material.h
@@ -36,18 +36,22 @@ private:
 
 class metal : public material {
 public:
-  metal(const color &albedo) : albedo(albedo) {}
+  metal(const color &albedo, double fuzz)
+      : albedo(albedo), fuzz(fuzz < 1 ? fuzz : 1) {}
 
   virtual bool scatter(const ray &r_in, const hit_record &rec,
                        color &attenuation, ray &scattered) const override {
     vec3 reflected = reflect(r_in.direction(), rec.normal);
+    reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
     scattered = ray(rec.p, reflected);
     attenuation = albedo;
-    return true;
+    // Ensure the scattered ray is in the same hemisphere as the normal
+    return (dot(scattered.direction(), rec.normal) > 0);
   }
 
 private:
   color albedo;
+  double fuzz;
 };
 
 #endif
