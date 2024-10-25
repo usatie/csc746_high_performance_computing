10b6f3f1da01a0114773b99859fcafc8e3ef0545
Shun Usami <usatie@gmail.com>
2024-10-25 16:50:47 -0700

    Add job script and execution log for OpenMP version of the ray tracer.

--- src/main.cpp
+++ src/main.cpp
@@ -90,7 +90,7 @@ void setup_world_cover(hittable_list &world, camera &cam) {
 
   cam.aspect_ratio = 16.0 / 9.0;
   cam.image_width = 1200;
-  cam.samples_per_pixel = 50;
+  cam.samples_per_pixel = 5;
   cam.max_depth = 50;
 
   cam.vfov = 20;
