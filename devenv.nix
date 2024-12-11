e1b98ab9f97d1051bbb895f213fd6765d6b395ce
Shun Usami <usatie@gmail.com>
2024-12-10 18:55:24 -0800

    Add plot script to generate speedup comparison chart

--- devenv.nix
+++ devenv.nix
@@ -7,13 +7,29 @@
   	pkgs.gcc13    # Adding gcc 13
   	pkgs.openblas # Adding OpenBLAS package
     pkgs.SDL2     # Adding SDL2 package
+	pkgs.ffmpeg   # Adding ffmpeg package
   ];
 
   languages.cplusplus = {
   	enable = true;
   };
+  languages.python = {
+  	enable = true;
+  	version = "3.9.1";
+  	venv = {
+  		 enable = true;
+  		 requirements = ''
+  			numpy --no-binary=numpy
+  			pandas
+  			matplotlib
+  			seaborn
+  		 '';
+  	};
+  };
   env = {
       "OPENBLAS" = "1";
+      "NUMPY_BLAS_LIB" = "openblas";
+      "NUMPY_LAPACK_LIB" = "openblas";
   	  "CXX"="${pkgs.gcc13}/bin/g++";
 	  "SDL2_CONFIG"="${pkgs.SDL2}/bin/sdl2-config";
 	  "SDL2_PATH"="${pkgs.SDL2}";
